40bfad00f8d2696207b37a8f078a04d2d1a5028c
Ampersander <romain-b49460@hotmail.com>
2021-01-18 10:37:40 +0100

    'Aync

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	import "@polymer/paper-card";
+	import "@material/mwc-button";
+	import '@material/mwc-top-app-bar-fixed';
+	import '@material/mwc-icon-button';
+	
+	let elements = [];
+	import DB from "./DB.svelte";
+</script>
+	
+	
+	<style>
+		paper-card{
+			width:90%;
+			margin-top: 3px;
+	margin-bottom: 3px;
+		}
+
+
+
+	</style>
+
+	​​
+	
+	<DB bind:elements = {elements} />
+	<link href="https://fonts.googleapis.com/css?family=Material+Icons&display=block" rel="stylesheet">
+	<mwc-top-app-bar-fixed>
+	<mwc-icon-button icon="menu" slot="navigationIcon"></mwc-icon-button>
+	<div slot="title">Title</div>
+		<mwc-icon-button icon="file_download" slot="actionItems"></mwc-icon-button>
+		<mwc-icon-button icon="print" slot="actionItems"></mwc-icon-button>
+		<mwc-icon-button icon="favorite" slot="actionItems"></mwc-icon-button>
+	<div><!-- content --></div>
+	</mwc-top-app-bar-fixed>
+	<main>
+		{#each elements as elem}​​
+		<paper-card heading={elem.title}​​>
+			<img src="data:image/jpeg;base64, {elem.img.data}​​" alt="{elem.title}​​" title="{elem.title}​​"/>
+			<div class="card-content">de {elem.author}​​ au prix de {elem.price}​​</div>
+		</paper-card>
+		{/each}​​
+		<mwc-button label="Click me!" />
+	</main>
---
+++ src/DB.svelte
@@ -0,0 +1,10 @@
+<script>
+
+    export let elements;
+    import {onMount} from 'svelte';
+
+    onMount(async () => {
+        const res = await fetch("books.json");
+        elements =  await res.json();
+    });
+</script>
